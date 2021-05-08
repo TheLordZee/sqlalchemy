188ac9757fb6551e7cd8ea13fb8f669a591454e5
LordZee <l0rclz33@gmail.com>
2021-05-08 14:09:51 -0400

    finished part 2

--- app.py
+++ app.py
@@ -2,25 +2,28 @@
 
 from flask import *
 from models import *
+from datetime import *
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'h7f4oubh487fbybqp8YE9P'
 
 connect_db(app)
 db.create_all()
 
 @app.route('/')
-def reroute():
-    return redirect('/users')
+def show_home():
+    posts = Post.query.order_by(Post.created_at.desc()).limit(5)
+    return render_template('home.html', posts=posts)
 
 @app.route('/users')
 def show_list():
     """Shows List of users"""
 
     users = User.query.all()
-    return render_template('home.html', users=users)
+    return render_template('users.html', users=users)
 
 
 @app.route('/users/new-user')
@@ -38,6 +41,14 @@ def make_user():
     url = request.form['img-url']
     if url == '':
         url = "/static/default.png"
+        if first_name == '':
+            flash('First Name Required')
+    
+    if last_name == '':
+        flash('Last Name required')
+
+    if (first_name == "") or (last_name == ""):
+        return redirect(f'/users/new-user')
 
     new_user = User(first_name=first_name, 
                     last_name=last_name, 
@@ -53,7 +64,8 @@ def show_user(user_id):
     """Shows detail about a user"""
 
     user = User.query.get_or_404(user_id)
-    return render_template('user.html', user=user)
+    posts = Post.query.filter(Post.user_id==user_id).all()
+    return render_template('user.html', user=user, posts=posts)
 
 @app.route('/users/<int:user_id>/edit')
 def edit_form(user_id):
@@ -71,6 +83,15 @@ def edit_user(user_id):
     url = request.form['img-url']
     if url == '':
         url = "/static/default.png"
+
+    if first_name == '':
+        flash('First Name Required')
+    
+    if last_name == '':
+        flash('Last Name required')
+
+    if (first_name == "") or (last_name == ""):
+        return redirect(f'/users/{user_id}/edit')
     
     user_edit = User.query.get(user_id)
     user_edit.first_name = first_name
@@ -89,4 +110,83 @@ def delete_user(user_id):
     User.query.filter(User.id == user_id).delete()
     db.session.commit()
 
-    return redirect('/')
+    return redirect('/users')
+
+@app.route('/posts/<int:post_id>')
+def show_post(post_id):
+    """Displays Post """
+
+    post = Post.query.get(post_id)
+    return render_template('post.html', post=post)
+
+@app.route('/users/<int:user_id>/posts/new')
+def show_post_form(user_id):
+    """Shows form to make new post"""
+
+    user = User.query.get_or_404(user_id)
+    return render_template('post-form.html', user=user)
+
+@app.route('/users/<int:user_id>/posts/new', methods=['POST'])
+def add_post(user_id):
+    """Saves the new post"""
+
+    title = request.form['title-form']
+    content = request.form['content-form']
+    if title == '':
+        flash('Title Required')
+    
+    if content == '':
+        flash('Content required')
+
+    if (content == "") or (title == ""):
+        return redirect(f'/users/{user_id}/posts/new') 
+
+    new_post = Post(title=title, content=content, user_id=user_id)
+
+    db.session.add(new_post)
+    db.session.commit()
+
+    return redirect(f'/users/{user_id}')
+
+@app.route('/posts/<int:post_id>/edit')
+def show_edit_form(post_id):
+    """Displays form to edit post"""
+
+    post = Post.query.get(post_id)
+    return render_template('post-edit-form.html', post=post)
+
+@app.route('/posts/<int:post_id>/edit', methods=['POST'])
+def edit_post(post_id):
+    """Saves the edit"""
+
+    title = request.form['title-form']
+    content = request.form['content-form']
+    if title == '':
+        flash('Title Required')
+    
+    if content == '':
+        flash('Content required')
+
+    if (content == "") or (title == ""):
+        return redirect(f'/posts/{{post_id}}/edit') 
+
+    edit_post = Post.query.get(post_id)
+    edit_post.title = title
+    edit_post.content = content
+
+    db.session.add(edit_post)
+    db.session.commit()
+
+    return redirect(f'/posts/{post_id}')
+
+@app.route('/posts/<int:post_id>/delete')
+def delete_post(post_id):
+    """Deletes post"""
+
+    post = Post.query.get(post_id)
+    user_id = post.user_id
+
+    Post.query.filter(Post.id == post_id).delete()
+    db.session.commit()
+
+    return redirect(f'/users/{user_id}')
--- models.py
+++ models.py
@@ -1,12 +1,14 @@
 """Models for Blogly."""
 from flask_sqlalchemy import SQLAlchemy
+from datetime import *
+
+# curr_time = now.strftime("%a %b %-d %Y, %-I:%M%P")
 
 db = SQLAlchemy()
 
 def connect_db(app):
     db.app = app
     db.init_app(app)
-
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -31,5 +33,36 @@ class User(db.Model):
     img_url = db.Column(db.String(),
                         default='/static/default.png')
 
+    posts = db.relationship('Post', 
+                            passive_deletes=True,
+                            backref='user')
+
     def get_full_name(self):
         return f"{self.first_name} {self.last_name}"
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    def __repr__(self):
+        p = self
+        return f"<Post {p.id}, title={p.title}, created_at={p.created_at}, user={p.user.get_full_name()}>"
+
+    id = db.Column(db.Integer,
+                    primary_key=True,
+                    autoincrement=True)
+
+    title = db.Column(db.Text,
+                        nullable=False)
+
+    content = db.Column(db.Text,
+                        nullable=False)
+
+    created_at = db.Column(db.DateTime,
+                            nullable=False,
+                            default = datetime.now())
+
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey('users.id', 
+                                    ondelete="CASCADE"))
+    
+    
